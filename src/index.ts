085db6a54d919e67fe7696aa8ec08f5c5fff1911
jgcamiloaga <jcamiloc148@gmail.com>
2026-03-08 15:43:30 -0500

    refactor: remove comments and clean up code structure in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,11 @@ import { config } from './config/env';
 import { setupReadyEvent } from './events/ready';
 import { setupInteractionCreateEvent } from './events/interactionCreate';
 
-// 1. Inicializamos el cliente del bot
 const client = new Client({
     intents: [GatewayIntentBits.Guilds],
 });
 
-// 2. Cargamos los eventos modularizados
 setupReadyEvent(client);
 setupInteractionCreateEvent(client);
 
-// 3. Conectar a Discord
 client.login(config.discordToken);
